be79e8bc212e167217c178bcdc1de6b228d45828
vijaybiloo <dpk.vijay@gmail.com>
2024-05-27 11:29:06 +0530

    created mongodb file

---
+++ mongodb.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DATE=$(date +%f)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$0/$DATE.log
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R Errror:: You should be the ROOT user to run this command $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e $2 ... $G SUCCESS $N"
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Coping mongo.repo file in to yum.repos.d/mongo.repo"
+
+yum install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "Installing Mongodb"
+
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "Starting mongodb"
+
+sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "Replacing 127.0.0.0 to 0.0.0.0 in mongod.conf"
+
+systemctl restart mongod &>> $LOGFILE
+VALIDATE $? "Restarting mongodb"
